bab9c0b68c50f503d280b4c965565f18f8297f67
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-05-07 19:23:38 +0300

    Add the certificate to the retrofit to make the communication encrypted using TLS and authenticate the server with the serverFingerprint

--- app/src/main/java/org/pacs/pacs_mobile_application/data/BackEndClient.java
+++ app/src/main/java/org/pacs/pacs_mobile_application/data/BackEndClient.java
@@ -1,35 +1,125 @@
 package org.pacs.pacs_mobile_application.data;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.Log;
+
+import org.pacs.pacs_mobile_application.R;
 import org.pacs.pacs_mobile_application.pojo.requestmodel.LoginModel;
 import org.pacs.pacs_mobile_application.pojo.requestmodel.RegistrationModel;
 import org.pacs.pacs_mobile_application.pojo.responsemodel.EmployeeAttributesModel;
 import org.pacs.pacs_mobile_application.pojo.responsemodel.UserInfoModel;
 import org.pacs.pacs_mobile_application.pojo.responsemodel.VisitorAttributesModel;
 
+import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Objects;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
-import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public class BackEndClient {
 
-    private static final String BASE_URL = "http://192.168.1.7:8084/pacs/";
+    private static Context applicationContext;
+    private static final String BASE_URL = "https://192.168.1.7:8084/pacs/";
     private final BackEndApi backEndApi;
     private static BackEndClient INSTANCE;
 
-    public BackEndClient() {
+    public BackEndClient(Context context) {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        try {
+
+            InputStream inputStream = context.getResources().openRawResource(R.raw.pacs_system);
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            X509Certificate certificate = (X509Certificate) cf.generateCertificate(inputStream);
+
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("certificate", certificate);
+
+            // Create a trust manager that trusts the self-signed certificate
+            @SuppressLint("CustomX509TrustManager") TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @SuppressLint("TrustAllX509TrustManager")
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                            // Verify that the server certificate is the one you expect
+                            X509Certificate certificate = chain[0];
+                            String serverFingerprint = sha1Fingerprint(certificate);
+                            String expectedFingerprint = "03:89:06:68:3F:E1:D2:70:75:C1:4A:EC:B1:45:81:F8:A5:38:D8:59";
+                            if (!Objects.requireNonNull(serverFingerprint).equalsIgnoreCase(expectedFingerprint)) {
+                                throw new CertificateException("Server certificate is not trusted.");
+                            }
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[0];
+                        }
+                    }
+            };
+
+            // Install the trust manager
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            httpClient.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0]);
+            httpClient.hostnameVerifier((hostname, session) -> true);
+
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build()) // Set the custom OkHttpClient
                 .build();
+
         backEndApi = retrofit.create(BackEndApi.class);
     }
+    private static String sha1Fingerprint(X509Certificate certificate) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] der = certificate.getEncoded();
+            md.update(der);
+            byte[] digest = md.digest();
+            StringBuilder fingerprint = new StringBuilder();
+            for (byte b : digest) {
+                if (fingerprint.length() > 0) {
+                    fingerprint.append(":");
+                }
+                fingerprint.append(String.format("%02X", b));
+            }
+            return fingerprint.toString();
+        } catch (CertificateEncodingException | NoSuchAlgorithmException e) {
+            Log.e("sha1Fingerprint","Failed to get the Fingerprint form the certificate");
+            return null;
+        }
+    }
 
-    public static BackEndClient getINSTANCE() {
+    public static BackEndClient getINSTANCE(Context context) {
         if (null == INSTANCE){
-            INSTANCE = new BackEndClient();
+            INSTANCE = new BackEndClient(context);
         }
         return INSTANCE;
     }
